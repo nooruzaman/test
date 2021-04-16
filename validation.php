b851ed78afb9879675add8b8a9a81c7028ad43d1
nooruzaman <35187802+nooruzaman@users.noreply.github.com>
2021-04-17 02:13:14 +1000

    Add files via upload

--- validation.php
+++ validation.php
@@ -1,27 +1,6 @@
 <?php
-session_start();
-try{
-	$connection=mysqli_connect('localhost','root', 'root', 'user_accounts');
-}
-catch(Exception $e) {
-  echo 'Message: ' .$e->getMessage();
-}
 
-//mysqli_select_db($connection, 'user_accounts');
-$name = $_POST['user'];
-$pwd  = $_POST['password'];
-$sql_query=" select * from user where username='$name' && password='$pwd' ";
-$sql_result=mysqli_query($connection,$sql_query);
-$num=mysqli_num_rows($sql_result);
-if ($num==1){
-	$row = mysqli_fetch_array($sql_result);
-	$_SESSION['username']= $row['username'];
-	header('location:home.php');
-}
-else
-{
-	$_SESSION['error']= "Login failed ... Username and/or Password not correct!";
-	//header('location:login.php');
-}
+  echo "<h1>Validation page running</h1>";
+
 
 ?>
