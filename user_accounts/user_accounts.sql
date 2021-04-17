eac6c48891e41902fc239eecd3973ba17f943850
nooruzaman <35187802+nooruzaman@users.noreply.github.com>
2021-04-17 20:43:19 +1000

    Add files via upload

--- user_accounts/user_accounts.sql
+++ user_accounts/user_accounts.sql
@@ -26,7 +26,7 @@ CREATE TABLE `user` (
   `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   PRIMARY KEY (`username`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
