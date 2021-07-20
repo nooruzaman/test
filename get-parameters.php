981bf67f329176251739af19f6b61e8cb96eb102
nooruzaman <35187802+nooruzaman@users.noreply.github.com>
2021-07-20 12:05:02 +1000

    Add files via upload

---
+++ get-parameters.php
@@ -0,0 +1,39 @@
+<?php
+  # Retrieve settings from Parameter Store
+  error_log('Retrieving settings');
+  require 'aws.phar';
+  
+  $az = file_get_contents('http://169.254.169.254/latest/meta-data/placement/availability-zone');
+  $region = substr($az, 0, -1);
+  $ssm_client = new Aws\Ssm\SsmClient([
+     'version' => 'latest',
+     'region'  => $region
+  ]);
+  
+  try {
+    # Retrieve settings from Parameter Store
+    $result = $ssm_client->GetParametersByPath(['Path' => '/example/', 'WithDecryption' => true]);
+
+    # Extract individual parameters
+    foreach($result['Parameters'] as $p) {
+        $values[$p['Name']] = $p['Value'];
+    }
+
+    $ep = $values['/example/endpoint'];
+    $un = $values['/example/username'];
+    $pw = $values['/example/password'];
+    $db = $values['/example/database'];
+    $ep = 'research-organization-database.c7ab3it6gt4u.us-east-1.rds.amazonaws.com';
+    $db = 'country_schema';
+    $un = 'admin';
+    $pw = 'PasswordForDB1234';
+	
+  }
+  catch (Exception $e) {
+    $ep = '';
+    $db = '';
+    $un = '';
+    $pw = '';
+  }
+
+?>
